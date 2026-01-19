eef1a5bb6f9835fac857e8075c6955c9ff2dd0d9
ANGOTThomas <128131883+ANGOTThomas@users.noreply.github.com>
2026-01-19 16:12:15 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,74 +1,43 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    basic.showString("" + (receivedNumber))
     if (receivedNumber == 1) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
-        RECULE()
+        AVANCE()
     }
     if (receivedNumber == 2) {
-        basic.showLeds(`
-            . . # # .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-            `)
         RECULE()
     }
     if (receivedNumber == 3) {
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
         STOP()
     }
     if (receivedNumber == 4) {
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
         DROITE()
     }
     if (receivedNumber == 5) {
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
         GAUCHE()
     }
 })
 function DROITE () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 170)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 0)
+    // motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, 170)
+    // motor.MotorRun(motor.Motors.M4, motor.Dir.CW, 170)
+    robobit.rotate(RBRobotDirection.Right, 60)
 }
 function GAUCHE () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 0)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 170)
-}
-function AVANCE2 () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 170)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 170)
+    // motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 170)
+    // motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, 170)
+    robobit.rotate(RBRobotDirection.Left, 60)
 }
 function STOP () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 0)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 0)
+    // motor.motorStopAll()
+    robobit.stop(RBStopMode.Coast)
 }
 function RECULE () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 170)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 170)
+    // motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, 170)
+    // motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, 170)
+    robobit.go(RBDirection.Reverse, 60)
+}
+function AVANCE () {
+    // motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 170)
+    // motor.MotorRun(motor.Motors.M4, motor.Dir.CW, 170)
+    robobit.go(RBDirection.Forward, 60)
 }
 radio.setGroup(82)
+robobit.select_model(RBModel.Mk3)
